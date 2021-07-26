22bafc4809b610ff23050126e27dfe91295c87ae
radharukminiY <rukku558@gmail.com>
2021-07-26 20:25:33 +0530

    commit 12

--- src/Component/Booking/bookingApi.js
+++ src/Component/Booking/bookingApi.js
@@ -1,26 +1,50 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
-import BookingDisplay from './bookingDisplay';
+import BookingDisplay from './bookingDetails';
 
-const url="http://localhost:7600/bookings"
+const url = "http://localhost:7600/bookings"
 
-class ViewBooking extends Component{
-  constructor(){
-    super()
-    this.state={
-      booking:''
+class ViewBooking extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            booking:''
+        }
     }
-  }
     render(){
         return(
-        <div>
-        <BookingDisplay bookdata={this.state.booking}/>
-        </div>
-      )
-        }
-        componentDidMount(){
-          axios.get(url).then((res) => {this.setState({booking:res.data})})
-        }
+            <div>
+                <BookingDisplay bookdata={this.state.booking}/>
+            </div>
+        )
     }
+
+    // api and patch response after payment
+    componentDidMount(){
+        if(this.props.location){
+            var qparams = this.props.location.search;
+            if(qparams){
+                console.log(qparams)
+                console.log(qparams.split('&')[0].split('='))
+                var data = {
+                    "status":qparams.split('&')[0].split('=')[1],
+                    "date":qparams.split('&')[2].split('=')[1],
+                    "bank":qparams.split('&')[3].split('=')[1]
+                }
+                fetch(`${url}/${qparams.split('&')[1].split('=')[1].split('_')[1]}`,
+                {
+                    method:'PATCH',
+                    headers:{
+                        'Accept':'application/json',
+                        'Content-Type':'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+            }
+        }
+        axios.get(url).then((res) => {this.setState({booking:res.data})})
+    }
+}
 
 export default ViewBooking;
--- src/Component/Booking/placeBooking.js
+++ src/Component/Booking/placeBooking.js
@@ -1,90 +1,87 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+const url = "http://localhost:7600/bookings";
 
-//import {Link} from 'react-router-dom';
-//import {Tab,Tabs,TabList,TabPanel} from 'react-tabs';
+class PlaceBooking extends Component {
+    constructor(props) {
+        super(props);
 
- const BookingUrl="http://localhost:7600/bookings"
-
-class PlaceBooking extends Component{
-  constructor(props){
-    super(props)
-
-    this.state={
-      id:Math.floor(Math.random()*1000),
-      hotel_name:this.props.match.params.hotel_name,
-      name:sessionStorage.getItem('username')?sessionStorage.getItem('username'):'',
-      phone:'',
-      count:''
+        this.state={
+            id:Math.floor(Math.random()*1000000),
+            hotel_name:this.props.match.params.hotel_name,
+            name:'',
+            phone:'',
+            email:'',
+            cost:sessionStorage.getItem('cost')
+        }
     }
-  }
-  handleChange=(event) =>{
-    this.setState({[event.target.name]:event.target.value})
-  }
-  handleSubmit=() =>{
-    console.log(this.state)
-    fetch(BookingUrl,
-    {
-      method:'POST',
-      headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify(this.state)
-    })
-    .then(this.props.history.push('/viewBooking'))
-  }
-  render(){
-    return(
-      <div className="container">
-      <div className="panel panel-info">
-      <div className="panel-heading">
-      place Booking
-      </div>
-      <div className="panel-body">
-      <div className="form-group">
-      <label>Order Id</label>
-      <input type="text" name="id" value={this.state.id} readOnly
-      className="form-control"/>
-      </div>
 
-      <div className="form-group">
-      <label>Hotel Name</label>
-      <input type="text" name="hotel_name" value={this.state.hotel_name} readOnly
-      className="form-control"/>
-      </div>
+    handleChange = (event) => {
+        this.setState({[event.target.name]:event.target.value})
+    }
 
-      <div className="form-group">
-      <label>Name</label>
-      <input type="text" name="name" value={this.state.name}
-      className="form-control"
-       onChange={this.handleChange}/>
-      </div>
-
-      <div className="form-group">
-      <label>phone</label>
-      <input type="text" name="phone" value={this.state.phone}
-      className="form-control"
-      className="form-control"
-       onChange={this.handleChange}/>
-      </div>
-      <div className="form-group">
-       <label>Number of Person</label>
-       <select name="count" value={this.state.count}
-        className="form-control"
-        onChange={this.handleChange}>
-      <option value="1">1</option>
-      <option value="2">2</option>
-       <option value="3">3</option>
-        </select>
-      </div>
-      <button className="btn btn-danger"
-         onClick={this.handleSubmit}>
-         Place Booking
-       </button>
-      </div>
-      </div>
-</div>
-    )
-  }
+    handleSubmit = () => {
+        console.log(this.state)
+        fetch(url,
+            {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state)
+            })
+            .then(console.log('order Placing'))
+    }
+    render(){
+        return(
+            <div className="container">
+                <div className="panel panel-info">
+                    <div className="panel-heading">
+                        Place Booking
+                    </div>
+                    <div className="panel-body">
+                        <form method="POST" action="https://developerpayment.herokuapp.com/paynow">
+                            <div className="form-group">
+                                <label>Order Id</label>
+                                <input readOnly name="id" value={this.state.id}
+                                className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Hotel Name</label>
+                                <input readOnly name="hotel_name" value={this.state.hotel_name}
+                                className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Cost</label>
+                                <input readOnly name="cost" value={this.state.cost}
+                                className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input name="name" value={this.state.name}
+                                className="form-control" onChange={this.handleChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input name="email" value={this.state.email}
+                                className="form-control" onChange={this.handleChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Phone</label>
+                                <input name="phone" value={this.state.phone}
+                                className="form-control" onChange={this.handleChange}/>
+                            </div>
+                            <button className="btn btn-success"
+                            type="submit"
+                             onClick={this.handleSubmit}>
+                                Pay Now
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
+
 export default PlaceBooking;
